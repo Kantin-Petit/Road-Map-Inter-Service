a781ec0aa7eeac65ec94b907385fd66d8e75fd8f
Damiennth <damien_thery@etu-u.bourgogne.fr>
2023-11-19 17:42:47 +0100

    first commit

---
+++ backend/routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const userCtrl = require('../controllers/user');
+const multer = require('../middleware/multer-config')
+const auth = require('../middleware/auth')
+
+router.get('/',auth, userCtrl.getAllUsers);
+router.get('/:id',auth, userCtrl.getOneUser);
+router.put('/active/:id',auth, userCtrl.makeAdmin); 
+router.put('/:id',auth, userCtrl.modifyUser);
+router.delete('/:id',auth, userCtrl.deleteUser);
+
+module.exports = router;
