a080dece117237d88f6f65e124d0a889c3aea837
Kantin-Petit <kantin.dev@gmail.com>
2023-12-01 11:36:18 +0100

    feat: add public/playout

--- frontend/src/app/public/public.module.ts
+++ frontend/src/app/public/public.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimelineCommunicationComponent } from './timeline-communication/timeline-communication.component';
 import { TimelinePlanningComponent } from './timeline-planning/timeline-planning.component';
+import { PlayoutComponent } from './playout/playout.component';
 
 
 
 @NgModule({
   declarations: [
     TimelineCommunicationComponent,
-    TimelinePlanningComponent
+    TimelinePlanningComponent,
+    PlayoutComponent
   ],
   imports: [
     CommonModule
