d3cc2f9a72c11ba2a74a8d03247a79ecfa2b0321
Kantin <kantinpetit@gmail.com>
2023-12-01 12:35:58 +0100

    feat: add admin/dashboard

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AlayoutComponent } from './alayout/alayout.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AlayoutComponent,
-    SidemenuComponent
+    SidemenuComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
