52edc4a502555e0343eac67a56e2bddfa2d8c655
Kantin <kantinpetit@gmail.com>
2023-12-01 14:45:43 +0100

    feat: configure guard

--- frontend/src/app/_helper/auth.guard.ts
+++ frontend/src/app/_helper/auth.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+// auth-guard.service.ts
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+      this.router.navigate(['/auth']);
+      return false;
+  }
+}
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './_utils/error/error.component';
+import { AuthGuard } from './_helper/auth.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
 
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate:[AuthGuard] },
 
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
 
