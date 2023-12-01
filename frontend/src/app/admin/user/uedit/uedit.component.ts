f633700ea080d9e973b7792d7d4ee2a9edbb0f53
Kantin-Petit <kantin.dev@gmail.com>
2023-12-01 12:11:11 +0100

    feat: add admin/user/uedit

---
+++ frontend/src/app/admin/user/uedit/uedit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uedit',
+  templateUrl: './uedit.component.html',
+  styleUrls: ['./uedit.component.scss']
+})
+export class UeditComponent {
+
+}
