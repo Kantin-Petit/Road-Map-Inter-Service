d610fe53eb40db0f9f697df1b78d8c4c227d455e
Kantin-Petit <kantin.dev@gmail.com>
2023-12-01 15:58:51 +0100

    feat: add intefaces

--- frontend/src/app/_service/auth.service.ts
+++ frontend/src/app/_service/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ICredential } from '../_interfaces/credential';
+import { IToken } from '../_interfaces/token';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  url='http:localhost:8888/auth/login'
+
+  constructor(private http: HttpClient) { }
+
+  login(credential: ICredential): Observable<IToken>{
+    return this.http.post<IToken>(this.url, credential)
+  }
 }
