d5f8ac2c09df54f3699b13729c7263c21bf9c02f
Kantin-Petit <kantin.dev@gmail.com>
2023-12-01 12:16:38 +0100

    feat: add admin/service/sindex

--- frontend/src/app/admin/service/service.module.ts
+++ frontend/src/app/admin/service/service.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ServiceRoutingModule } from './service-routing.module';
+import { SindexComponent } from './sindex/sindex.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SindexComponent
+  ],
   imports: [
     CommonModule,
     ServiceRoutingModule
