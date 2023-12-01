2d8f7da1b084f4cba27c83dc05dd0dea9406f05e
Kantin <kantinpetit@gmail.com>
2023-12-01 12:35:03 +0100

    feat: add Admin.router

--- frontend/src/app/admin/service/service-routing.module.ts
+++ frontend/src/app/admin/service/service-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SindexComponent } from './sindex/sindex.component';
+import { SeditComponent } from './sedit/sedit.component';
+import { SaddComponent } from './sadd/sadd.component';
+import { SdeleteComponent } from './sdelete/sdelete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SindexComponent },
+  { path: 'edit/:id', component: SeditComponent},
+  { path: 'add', component: SaddComponent},
+  { path: 'delete/:id', component: SdeleteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
