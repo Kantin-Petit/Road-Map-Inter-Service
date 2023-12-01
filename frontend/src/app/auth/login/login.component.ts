7bd9cfc0b8407b30c2b2161227e6ee985849d570
Kantin-Petit <kantin.dev@gmail.com>
2023-12-01 16:14:20 +0100

    feat: add tokenService + link to guard

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ICredential } from 'src/app/_interfaces/credential';
 import { IToken } from 'src/app/_interfaces/token';
 import { AuthService } from 'src/app/_service/auth.service';
+import { TokenService } from 'src/app/_service/token.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,9 @@ import { AuthService } from 'src/app/_service/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private tokenService: TokenService) { }
 
   form: ICredential = {
     email: '',
@@ -19,7 +22,10 @@ export class LoginComponent {
   onSubmit(){
     console.log(this.form)
     this.authService.login(this.form).subscribe(
-      data  => console.log(data.token),
+      data  => {
+        console.log(data.token)
+        this.tokenService.saveToken(data.token)
+      },
       err => console.log(err)
     )
   }
