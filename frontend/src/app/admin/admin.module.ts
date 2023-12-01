d05b2b72471d6e01a1977c0b87af76f61f8de99e
Kantin <kantinpetit@gmail.com>
2023-12-01 12:06:20 +0100

    feat: add admin/alayout

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AlayoutComponent } from './alayout/alayout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AlayoutComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
