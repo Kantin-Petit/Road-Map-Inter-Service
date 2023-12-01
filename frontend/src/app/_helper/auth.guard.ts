b1bb2e46779fb787fe942dfa767c0948ad009129
Kantin <kantinpetit@gmail.com>
2023-12-01 16:14:20 +0100

    feat: add tokenService + link to guard

--- frontend/src/app/_helper/auth.guard.ts
+++ frontend/src/app/_helper/auth.guard.ts
@@ -2,15 +2,23 @@
 
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { TokenService } from '../_service/token.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private tokenService: TokenService
+    ) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-      this.router.navigate(['/auth']);
-      return false;
+    let test = this.tokenService.isLogged()  
+    if(test){
+      return true
+    }
+    this.router.navigate(['/auth'])
+    return false
   }
 }
