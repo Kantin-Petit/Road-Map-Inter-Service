9bbdacc40db42dddae1c21b5bc30ea3cd6d5abae
Kantin <kantinpetit@gmail.com>
2023-12-01 12:11:11 +0100

    feat: add admin/user/uedit

--- frontend/src/app/admin/user/user.module.ts
+++ frontend/src/app/admin/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UindexComponent } from './uindex/uindex.component';
+import { UeditComponent } from './uedit/uedit.component';
 
 
 @NgModule({
   declarations: [
-    UindexComponent
+    UindexComponent,
+    UeditComponent
   ],
   imports: [
     CommonModule,
