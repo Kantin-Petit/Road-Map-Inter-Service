5c29a335873fe2159515884c66887fdb22970ef2
Kantin-Petit <kantin.dev@gmail.com>
2023-12-01 11:35:52 +0100

    feat: add public/timeline-planning

--- frontend/src/app/public/public.module.ts
+++ frontend/src/app/public/public.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimelineCommunicationComponent } from './timeline-communication/timeline-communication.component';
+import { TimelinePlanningComponent } from './timeline-planning/timeline-planning.component';
 
 
 
 @NgModule({
   declarations: [
-    TimelineCommunicationComponent
+    TimelineCommunicationComponent,
+    TimelinePlanningComponent
   ],
   imports: [
     CommonModule
---
+++ frontend/src/app/public/timeline-planning/timeline-planning.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline-planning',
+  templateUrl: './timeline-planning.component.html',
+  styleUrls: ['./timeline-planning.component.scss']
+})
+export class TimelinePlanningComponent {
+
+}
