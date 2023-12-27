01bc97778bb2210e1092c625bcca3ba71f354b37
Damiennth <damien_thery@etu-u.bourgogne.fr>
2023-12-27 22:12:42 +0100

    add color on Timeline and turn select subject into checkbox

--- backend/routes/service.js
+++ backend/routes/service.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const serviceCtrl = require('../controllers/service');
 
-router.get('/filter', serviceCtrl.getFilteredServices);
 router.get('/:name', serviceCtrl.getOneservice);
 router.get('/', serviceCtrl.getAllservices);
+router.post('/filter', serviceCtrl.getFilteredServices);
 // router.post('/service', serviceCtrl.addService);
 // router.put('/service', serviceCtrl.modifyService);
 // router.delete('/service', serviceCtrl.deleteService);
