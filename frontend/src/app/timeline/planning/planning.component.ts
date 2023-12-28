efdd3e3d05a5802b1290e53f10876e48d5058a99
Damiennth <damien_thery@etu-u.bourgogne.fr>
2023-12-28 06:34:23 +0100

    add color and select to planning

--- frontend/src/app/timeline/planning/planning.component.ts
+++ frontend/src/app/timeline/planning/planning.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, HostListener  } from '@angular/core';
 import { Service } from 'src/app/models/service-model';
 import { Subject } from '../../models/subject-model';
 import { ServiceService } from 'src/app/services/service.service';
@@ -26,7 +26,12 @@ export class PlanningComponent implements OnInit, AfterViewInit {
   checkedServicesInit : { [key: string]: boolean } = {};
   checkedSubjects: { [key: string]: { [innerKey: string]: boolean } } = {};
 
+  sidebarVisible: boolean = false;
+  sidebarData: { title: string, text: string } = { title: '', text: '' };
+  selectTimeline!: string;
+
   @ViewChild('timeline', {static: false}) timelineContainer!: ElementRef;
+
 
   constructor(
     private serviceService: ServiceService,
@@ -51,12 +56,20 @@ export class PlanningComponent implements OnInit, AfterViewInit {
   }
 
   addDataIntoDom() {
-    const keys = Object.keys(this.services);
+    const servicesKeys = Object.keys(this.services);
     let compteur = 1;
-    for (let i = 0; i < keys.length; i++) {
-      this.groups.add({id: i, content: keys[i]});
-      for (let j = 0; j < this.services[keys[i]].timelines.length; j++) {
-        this.data.add({id: compteur, content: this.services[keys[i]].timelines[j].titre, start: this.services[keys[i]].timelines[j].dateStart, end: this.services[keys[i]].timelines[j].dateEnd, group: i});
+    for (let serviceIndex = 0; serviceIndex < servicesKeys.length; serviceIndex++) {
+      this.groups.add({id: serviceIndex, content: servicesKeys[serviceIndex],  className: "custom_group",});
+      for (let timelineIndex = 0; timelineIndex < this.services[servicesKeys[serviceIndex]].timelines.length; timelineIndex++) {
+        const Timeline = this.services[servicesKeys[serviceIndex]].timelines[timelineIndex];
+        this.data.add({
+          id: `service${serviceIndex}_timeline${timelineIndex}`, 
+          content: Timeline,
+          start: Timeline.dateStart,
+          end: Timeline.dateEnd,
+          className: `custom_item ${Timeline.sujet} service${serviceIndex}_timeline${timelineIndex}`,
+          group: serviceIndex
+        });
         compteur++;
       }
     } 
@@ -64,8 +77,22 @@ export class PlanningComponent implements OnInit, AfterViewInit {
 
   setSubjects() {
     this.subjectService.getAllsubject().subscribe(sujets => {
+      
       this.subjects = sujets;
+
+      const classes: string[] = Object.keys(this.subjects);
+      let cssStyles = '';
+
+      classes.forEach((className) => {
+          var color = this.getColorForSubject(className);
+          cssStyles += `.${className} { background-color: ${color}; }\n`;
+      });
+
+      let styleTag = document.createElement('style');
+      styleTag.textContent = cssStyles;
+      document.head.appendChild(styleTag);
     });
+
   }
 
   isAllChecked() {
@@ -134,20 +161,55 @@ export class PlanningComponent implements OnInit, AfterViewInit {
     this.timeline = new Timeline(this.timelineContainer.nativeElement, this.data, this.groups, this.options);
     this.timeline.setGroups(this.groups);
     this.timeline.setItems(this.data);
-    this.timeline.on('select', function (properties) {
-      alert('selected items: ' + properties.items);
+    this.timeline.on('click',  (properties) => {
+      if(!properties.event.target.parentNode['vis-item']?.data.content) return
+      const content = properties.event.target.parentNode['vis-item'];
+      this.toggleSidebar(content)
     });
   }
 
+  toggleSidebar(elt: any): void {
+
+    this.sidebarData = { title: '', text: '' };
+    this.selectTimeline = '';
+    const data = elt.data.content;
+
+    if (!this.sidebarVisible) {
+      this.sidebarData = { title: data.titre, text: data.texte };
+      this.sidebarVisible = true;
+      this.selectTimeline = elt.id
+      this.toogleActive()
+    } 
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if(this.selectTimeline && !this.sidebarVisible) document.querySelector(`.${this.selectTimeline}`)?.classList.remove("active");
+  }
+
+  toogleActive(){
+    const timeline = document.querySelector(`.${this.selectTimeline}`)
+    if(timeline) timeline.classList.add("active");
+  }
+
+  getColorForSubject(subjectName: string): string {
+    const foundSubject = this.subjects[subjectName];
+    return foundSubject ? foundSubject.color : '#000000';
+  }
+
+  isSingleText(texte: string): boolean {
+    return Array.isArray(texte);
+  }
+
   getOptions() {
+
     this.options = {
       selectable: false,
       locale: 'fr',
-      // template: function (item: any, element: any, data: any) {
-      //   var html = '<h1>' + item.content + '</h1>';
-      //   return html;
-      // },
-      
+      template: function (item: any, element: any, data: any) {},
     };
+
   }
+
+ 
 }
