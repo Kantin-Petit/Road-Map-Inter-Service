c11e4d8c86bd5df97c0f084283f61fab0e0ca7ef
Damiennth <damien_thery@etu-u.bourgogne.fr>
2024-01-26 13:57:22 +0100

    filter Done

--- frontend/src/app/dashboard/dashboard-routing.module.ts
+++ frontend/src/app/dashboard/dashboard-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminUserComponent } from './admin/admin-user/admin-user.component';
-// import { AdminServiceComponent } from './admin/admin-service/admin-service.component';
+import { AdminServiceComponent } from './admin/admin-service/admin-service.component';
 import { AdminThematicComponent } from './admin/admin-thematic/admin-thematic.component';
+import { AdminTimelineComponent } from './admin/admin-timeline/admin-timeline.component';
 import { DashboardComponent } from './dashboard.component';
 
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
-  // { path: 'services', component: AdminServiceComponent },
+  { path: 'services', component: AdminServiceComponent },
   { path: 'thematiques', component: AdminThematicComponent },
+  { path: 'timelines', component: AdminTimelineComponent },
   { path: 'utilisateurs', component: AdminUserComponent },
 ];
 
