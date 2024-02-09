c217791491a9c17ed563b79f4b795d1d0165089d
Kantin-Petit <kantin.dev@gmail.com>
2024-02-09 09:22:21 +0100

    fix any styles

--- frontend/src/app/timeline/planning/planning.component.ts
+++ frontend/src/app/timeline/planning/planning.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, HostListener }
 import { TimelineModel } from 'src/app/models/timeline-model';
 import { DataSet, Timeline } from "vis-timeline/standalone";
 import { FilterService } from 'src/app/services/filter.service';
+import vis from 'vis-network/declarations/index-legacy-bundle';
 
 @Component({
   selector: 'app-planning',
@@ -15,7 +16,7 @@ export class PlanningComponent implements OnInit, AfterViewInit {
   options!: {};
   data: any;
   groups: any;
-
+  
   @ViewChild('timeline', { static: false }) timelineContainer!: ElementRef;
 
   constructor(public filterService: FilterService) {
@@ -59,15 +60,54 @@ export class PlanningComponent implements OnInit, AfterViewInit {
         });
       });
     });
+
+    this.setStyle();
+
   }
 
+  setStyle() {
+    let cssStyles = '';
+
+    let custom_group = '.custom_group {';
+    // custom_group += 'height: 100px !important;';
+    custom_group += 'border: 1px solid #e5e5e5;';
+    custom_group += ' }';
+
+    let vis_label = '.vis-label {';
+    vis_label += 'text-align: center;';
+    vis_label += 'font-size: 1.3rem;';
+    vis_label += 'font-weight: bold;';
+    vis_label += ' }';
+
+    let timeline_title = '.timeline-title {';
+    timeline_title += 'margin: 0;';
+    timeline_title += '}';
+
+    let vis_content = '.vis-content { ';
+    vis_content += 'top: 0 !important;';
+    vis_content += 'transform: translateY(0) !important;';
+    vis_content += '';
+    vis_content += ' }';
+
+    let vis_item_content = '.vis-item-content { ';
+    vis_item_content += 'display: flex !important;';
+    vis_item_content += 'justify-content: space-between;';
+    vis_item_content += '}';
+
+    //let thematic_div = '.thematics div { width: 10px; height: 10px; margin: 0 5px; }';
+
+    cssStyles += custom_group + '\n' + vis_label + '\n'+ timeline_title + '\n' + vis_content + '\n' + vis_item_content + '\n'  + '\n';
+    let styleTag = document.createElement('style');
+    styleTag.textContent = cssStyles;
+    document.head.appendChild(styleTag);
+  }
 
   setThematics() {
 
       let cssStyles = '';
 
       this.filterService.thematics.forEach((thematic) => { 
-          let color = thematic.color;
+          let color = '#ffffff';
           cssStyles += `.${thematic.name} { background-color: ${color}; }\n`;
       });
 
@@ -131,7 +171,15 @@ export class PlanningComponent implements OnInit, AfterViewInit {
     this.options = {
       selectable: false,
       locale: 'fr',
-      template: function (item: any, element: any, data: any) { },
+      template: function (item: any, element: any, data: any) { 
+        return `
+        <div class="vis-item-content">
+          <div class="vis-label">${item.content.title}</div>
+        </div>
+        <style>
+          .vis-item-content { display: flex; justify-content: space-between; }
+        </style>`;
+       },
     };
 
   }
