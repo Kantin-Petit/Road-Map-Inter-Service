42e8f2ac6fe44743ef7ca643d85264bac85e0c00
Damiennth <damien_thery@etu-u.bourgogne.fr>
2024-02-22 15:20:38 +0100

    thematic done add new endpoint

--- backend/routes/thematicTimeline.js
+++ backend/routes/thematicTimeline.js
@@ -4,6 +4,7 @@ const thematicTimelineCtrl = require('../controllers/thematicTimeline');
 
 router.get('/:id', thematicTimelineCtrl.getAssociation);
 router.post('/', thematicTimelineCtrl.createAssociation);
+router.delete('/all/:type/:id', thematicTimelineCtrl.deleteAllAssociations);
 router.delete('/:timelineId/:thematicId', thematicTimelineCtrl.deleteAssociation);
 
 module.exports = router;
